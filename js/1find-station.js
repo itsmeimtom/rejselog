c311c10fbc58c09c68b2c032d6ccb4b1b8037726
Thomas Robinson <git@tomr.me>
2023-04-11 21:02:15 +0100

    enter to pick first station

--- js/1find-station.js
+++ js/1find-station.js
@@ -9,18 +9,24 @@ date.value = new Date().toISOString().slice(0, 16);
 departureStation.addEventListener("input", stepOneinputsChange);
 date.addEventListener("input", stepOneinputsChange);
 
-function stepOneinputsChange() {
+function stepOneinputsChange() {	
 	const value = departureStation.value;
-	stationList.innerHTML = value;
 
 	if (value.length < 3) return stationList.innerHTML = "<li>Type some more to search</li>";
+
+	stationList.innerHTML = `<li>Searching for ${value}</li>`;
 
 	clearTimeout(searchDebounce);
 	searchDebounce = setTimeout(() => {
 		stepOneListMatches(value);
 	}, 100);
-	
 }
+
+document.querySelector("form").addEventListener("submit", (e) => {
+	e.preventDefault();
+	
+	document.querySelector("ul#station-list li").click();
+});
 
 async function stepOneListMatches(name) {
 	const url = endpoint + "location?input=" + name + "&format=json";
